88326518ef2cf1fb2e510eb50ace85e38dd2108b
IBliind <pedrosantos147@hotmail.com>
2022-05-29 10:59:53 -0300

    commit inicial

---
+++ Series/Interfaces/IRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dio.Series.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Insere(T identidade);
+        void Exclui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+
+    }
+}
